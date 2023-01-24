b3de9bf63e9415172d101830f6f708d2f124c5f6
SamuelePassiatore <semmisno2@protonmail.com>
2023-01-24 16:00:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <header>
+    <h1 class="text-center py-3">Pokemon</h1>
+  </header>
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss'
+</style>
